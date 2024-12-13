550990d4fab65bda4d4417992ef4af3d89285c08
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-12-13 23:37:04 +0530

    change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => {
   return (
     <div>
       <h1 className='text-center text-red-500 text-5xl'>Blog Application</h1>
+      <h2 className='text-center text-orange-400 text-3xl'>first blog</h2>
     </div>
   )
 }
