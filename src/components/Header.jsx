cde1ada3c722a84ab9a5aea78dc4f7f00d0d9ed8
Tauqueer manzar <mdtauqueermanzar@gmail.com>
2024-12-13 23:33:09 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='text-center text-red-500 text-5xl'>Blog Application</h1>
+    </div>
+  )
+}
+
+export default Header
